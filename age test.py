771f7544cd0ca500b0a72a441f8b1141a13c2546
AdhritShetty <adhritshetty2009@gmail.com>
2025-04-07 20:17:34 +0000

    age test

---
+++ age test.py
@@ -0,0 +1,9 @@
+age=int(input("please enter the age:"))
+if age <=20:
+    print("eligble")
+    if age >=10:
+        print ("eligible")
+    else:
+        print ("not elgible")
+else:print("not eligible")
+        
